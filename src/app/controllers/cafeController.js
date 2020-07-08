c53a2f7cdd580aa23975e397a08632736e9d4842
won <022499@naver.com>
2020-07-09 03:54:13 +0900

    카페 완료

--- src/app/controllers/cafeController.js
+++ src/app/controllers/cafeController.js
@@ -4,7 +4,7 @@ const {logger} = require('../../../config/winston');
 
 const jwt = require('jsonwebtoken');
 
-// 카테고리 글 모아보기
+// 25. 카테고리 글 모아보기 ------------------------------------------------------------//
 
 exports.cateBoardList = async function (req, res) {
     const id= req.verifiedToken.id;
@@ -56,7 +56,7 @@ exports.cateBoardList = async function (req, res) {
     }
 };
 
-// 전체글 모아보기
+// 26. 전체글 모아보기 ------------------------------------------------------------//
 
 exports.boardList = async function (req, res) {
     const id= req.verifiedToken.id;
@@ -107,7 +107,7 @@ exports.boardList = async function (req, res) {
     }
 };
 
-//     app.get('/cafe/:board', jwtMiddleware, cafe.boardInfo);//상세글보기
+//    27. 상세글보기------------------------------------------------------------//
 
 exports.boardInfo = async function (req, res) {
     try {
@@ -162,7 +162,7 @@ exports.boardInfo = async function (req, res) {
     }
 };
 
-//  글작성
+//  28. 글작성  ------------------------------------------------------------//
 
 exports.createBoard = async function (req, res) {
     const id= req.verifiedToken.id;
@@ -199,7 +199,7 @@ exports.createBoard = async function (req, res) {
     }
 };
 
-//     app.route('/cafe/board').delete(jwtMiddleware, cafe.deleteBoard);//글 삭제
+//     29. 글 삭제  ------------------------------------------------------------//
 exports.deleteBoard = async function (req, res) {
     const id= req.verifiedToken.id;
     const bid= req.params.boardId;
@@ -235,10 +235,79 @@ exports.deleteBoard = async function (req, res) {
     }
 };
 
-// //글 좋아요
-// //글 북마크
+// 30. 글 좋아요 ------------------------------------------------------------//
 
-//댓글 작성
+
+exports.likeBoard = async function (req, res) {
+    const id= req.verifiedToken.id;
+    const bid = req.body.boardId;
+
+    try {
+        const connection = await pool.getConnection(async conn => conn);
+        try {            
+
+            const likeBoardQuery = `
+                insert likeInfo(userId, type, likeId) values
+                (?, 1, ?);
+                    `;
+            const likeBoardParams = [id, bid ];
+            await connection.query(likeBoardQuery, likeBoardParams);
+
+            await connection.commit(); // COMMIT
+            connection.release();
+
+            return res.json({
+                isSuccess: true,
+                code: 200,
+                message: "게시글 좋아요가 완료되었습니다"
+            });
+        } catch (err) {
+            connection.release();
+            logger.error(`App - like Board Query error\n: ${err.message}`);
+            return res.status(501).send(`Error: ${err.message}`);
+        }
+    } catch (err) {
+        logger.error(`App - like Board Connection error\n: ${err.message}`);
+        return res.status(502).send(`Error: ${err.message}`);
+    }
+};
+
+// 31. 글 북마크 ------------------------------------------------------------//
+exports.markBoard = async function (req, res) {
+    const id= req.verifiedToken.id;
+    const bid = req.body.boardId;
+
+    try {
+        const connection = await pool.getConnection(async conn => conn);
+        try {            
+
+            const markBoardQuery = `
+                insert bookmark(userId, boardId) values
+                (?, ?);
+                    `;
+            const markBoardParams = [id, bid];
+            await connection.query(markBoardQuery, markBoardParams);
+
+            await connection.commit(); // COMMIT
+            connection.release();
+            
+            return res.json({
+                isSuccess: true,
+                code: 200,
+                message: "게시글 북마크가 완료되었습니다"
+            });
+        } catch (err) {
+            connection.release();
+            logger.error(`App - mark Board Query error\n: ${err.message}`);
+            return res.status(501).send(`Error: ${err.message}`);
+        }
+    } catch (err) {
+        logger.error(`App - mark Board Connection error\n: ${err.message}`);
+        return res.status(502).send(`Error: ${err.message}`);
+    }
+};
+
+//32. 댓글 작성 ------------------------------------------------------------//
 
 exports.createComment = async function (req, res) {
     const id= req.verifiedToken.id;//회원id
@@ -275,7 +344,7 @@ exports.createComment = async function (req, res) {
     }
 };
 
-//   댓글 삭제
+//  33. 댓글 삭제 ------------------------------------------------------------//
 
 exports.deleteComment = async function (req, res) {
     const id= req.verifiedToken.id;
@@ -310,11 +379,120 @@ exports.deleteComment = async function (req, res) {
         return res.status(502).send(`Error: ${err.message}`);
     }
 };
-// //댓글 좋아요
-// //답글 달기
+// 34. 댓글 좋아요------------------------------------------------------------//
 
-//     app.get('/cafe/search', jwtMiddleware, cafe.search);//제목 검색
-//     app.get('/cafe/user', jwtMiddleware, cafe.boardUser);//작성한 게시물 보기
+exports.likeComment = async function (req, res) {
+    const id= req.verifiedToken.id;
+    const cid = req.body.commentId;
+
+    try {
+        const connection = await pool.getConnection(async conn => conn);
+        try {            
+
+            const likeCommentQuery = `
+                insert likeInfo(userId, type, likeId) values
+                (?, 2, ?);
+                    `;
+            const likeCommentParams = [id, cid ];
+            await connection.query(likeCommentQuery, likeCommentParams);
+
+            await connection.commit(); // COMMIT
+            connection.release();
+
+            return res.json({
+                isSuccess: true,
+                code: 200,
+                message: "댓글 좋아요가 완료되었습니다"
+            });
+        } catch (err) {
+            connection.release();
+            logger.error(`App - like Comment Query error\n: ${err.message}`);
+            return res.status(501).send(`Error: ${err.message}`);
+        }
+    } catch (err) {
+        logger.error(`App - like Comment Connection error\n: ${err.message}`);
+        return res.status(502).send(`Error: ${err.message}`);
+    }
+};
+
+// 35. 답글 달기 createRecomment------------------------------------------------------------//
+exports.createRecomment = async function (req, res) {
+    const id= req.verifiedToken.id;//회원id
+    const content= req.body.content;
+    const cid=req.body.commentId;
+
+    try {
+        const connection = await pool.getConnection(async conn => conn);
+        try {            
+
+            const insertRecommentQuery = `
+                insert recomment(userId, commentId, content) values
+                (?,?,?);
+                    `;
+            const insertRecommentParams = [id, cid, content];
+            await connection.query(insertRecommentQuery, insertRecommentParams);
+
+            await connection.commit(); // COMMIT
+            connection.release();
+
+            return res.json({
+                isSuccess: true,
+                code: 200,
+                message: "대댓글 작성이 완료되었습니다"
+            });
+        } catch (err) {
+            connection.release();
+            logger.error(`App - Insert Recomment Query error\n: ${err.message}`);
+            return res.status(501).send(`Error: ${err.message}`);
+        }
+    } catch (err) {
+        logger.error(`App - Insert Recomment Connection error\n: ${err.message}`);
+        return res.status(502).send(`Error: ${err.message}`);
+    }
+};
+
+//  36. 제목 검색 ------------------------------------------------------------//
+
+exports.search = async function (req, res) {
+
+    const id= req.verifiedToken.id;
+    const word = req.query.word;
+
+    try {
+        const connection = await pool.getConnection(async conn => conn);
+        try {
+
+            const searchQuery = `
+            
+                select title, concat(left(content, 20), '...')
+                from board
+                where title like '%${word}%' and status='ACTIVE';
+
+                    `;
+            const searchParams = [word];
+            const [search] = await connection.query(searchQuery, searchParams);
+
+            await connection.commit(); // COMMIT
+            connection.release();
+            return res.json({
+                result:search[0],
+                isSuccess: true,
+                code: 200,
+                message: "검색에 성공했습니다"
+            });
+        } catch (err) {
+            await connection.rollback(); // ROLLBACK
+            connection.release();
+            logger.error(`App - Search Query error\n: ${err.message}`);
+            return res.status(501).send(`Error: ${err.message}`);
+        }
+    } catch (err) {
+        logger.error(`App - Search DB Connection error\n: ${err.message}`);
+        return res.status(502).send(`Error: ${err.message}`);
+    }
+};
+
+//  37. 작성한 게시물 보기 ------------------------------------------------------------//
 
 exports.userBoard = async function (req, res) {
     const id= req.verifiedToken.id;
@@ -357,4 +535,42 @@ exports.userBoard = async function (req, res) {
 };
 
 
-// //북마크 목록 보기
+// 38. 북마크 목록 보기 ------------------------------------------------------------//
+exports.userMark = async function (req, res) {
+    const id= req.verifiedToken.id;
+    
+    try {
+        const connection = await pool.getConnection(async conn => conn);
+        try {
+            const getUserMarkQuery = `
+
+                select b.title #, b의 좋아요개수, 댓글 개수
+                from bookmark as m left join board as b on m.boardId=b.idx
+                where b.userId=? and (b.status='ACTIVE' and m.status='ACTIVE');
+                `    ;
+
+            const getUserMarkParams = [id];
+
+            const[getUserMarkRows]= await connection.query(getUserMarkQuery, getUserMarkParams);
+
+            await connection.commit(); // COMMIT
+            connection.release();
+            
+            res.json({
+                list:getUserMarkRows[0],
+                isSuccess: true,
+                code: 200,
+                message: "작성자 게시글 조회 성공"
+            });
+            
+        } catch (err) {
+            await connection.rollback(); // ROLLBACK
+            connection.release();
+            logger.error(`App - Get User Mark Query error\n: ${err.message}`);
+            return res.status(501).send(`Error: ${err.message}`);
+        }
+    } catch (err) {
+        logger.error(`App - Get User Mark DB Connection error\n: ${err.message}`);
+        return res.status(502).send(`Error: ${err.message}`);
+    }
+};
--- src/app/routes/cafeRoute.js
+++ src/app/routes/cafeRoute.js
@@ -2,20 +2,22 @@ module.exports = function(app){
     const cafe = require('../controllers/cafeController');
     const jwtMiddleware = require('../../../config/jwtMiddleware');
 
-    app.get('/cafe/cate', jwtMiddleware, cafe.cateBoardList);//카테고리 글 모아보기
-    app.get('/cafe/list', jwtMiddleware, cafe.boardList);//전체글 모아보기
-    app.get('/cafe/board', jwtMiddleware, cafe.boardInfo);//상세글보기
-    app.route('/cafe/board').post(jwtMiddleware,cafe.createBoard);//글작성
-    app.route('/cafe/board').delete(jwtMiddleware, cafe.deleteBoard);//글 삭제
-//글 좋아요
-//글 북마크
+    app.get('/cafe/cate', jwtMiddleware, cafe.cateBoardList);//카테고리 글 모아보기ㅇㄹ
+    app.get('/cafe/list', jwtMiddleware, cafe.boardList);//전체글 모아보기ㅇㄹ
+    app.get('/cafe/board', jwtMiddleware, cafe.boardInfo);//상세글보기ㅇㄹ
+    app.route('/cafe/board').post(jwtMiddleware,cafe.createBoard);//글작성ㅇㄹ
+    app.route('/cafe/board').delete(jwtMiddleware, cafe.deleteBoard);//글 삭제ㅇㄹ
 
-    app.route('/cafe/comment').post(jwtMiddleware,cafe.createComment);//댓글 작성
-    app.route('/cafe/comment').delete(jwtMiddleware,cafe.deleteComment);//댓글 삭제
-//댓글 좋아요
-//답글 달기
+    app.route('/cafe/likeboard').post(jwtMiddleware,cafe.likeBoard);//글 좋아요ㅇㄹ
+    app.route('/cafe/markboard').post(jwtMiddleware,cafe.markBoard);//글 북마크ㅇㄹ
 
-   // app.get('/cafe/search', jwtMiddleware, cafe.search);//제목 검색
-    app.get('/cafe/user', jwtMiddleware, cafe.userBoard);//작성한 게시물 보기
-//북마크 목록 보기
+    app.route('/cafe/comment').post(jwtMiddleware,cafe.createComment);//댓글 작성ㅇㄹ
+    app.route('/cafe/comment').delete(jwtMiddleware,cafe.deleteComment);//댓글 삭제ㅇㄹ
+
+    app.route('/cafe/likecomment').post(jwtMiddleware,cafe.likeComment);//댓글 좋아요ㅇㄹ
+    app.route('/cafe/recomment').post(jwtMiddleware,cafe.createRecomment);//답글 달기ㅇㄹ
+
+    app.get('/cafe/search', jwtMiddleware, cafe.search);//제목 검색ㅇㄹ
+    app.get('/cafe/user', jwtMiddleware, cafe.userBoard);//작성한 게시물 보기ㅇㄹ
+    app.get('/cafe/usermark', jwtMiddleware, cafe.userMark);//북마크 목록 보기ㅇㄹ
 };
